451172950cb58a600eb88512d3b805287e81d55e
kmrkva <43647427+kmrkva@users.noreply.github.com>
2025-02-07 09:55:51 -0600

    fixing check is undefined

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image"
 import { useState, useEffect, useRef } from "react"
 import { useRouter } from "next/navigation"
-import { Battery, Camera, Cpu, ZoomIn, Wifi, Usb } from "lucide-react"
+import { Battery, Camera, Cpu, ZoomIn, Wifi, Usb, Check } from "lucide-react"
 import { type LucideIcon } from 'lucide-react'
 
 export default function CompareIPhones() {
